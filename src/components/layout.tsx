170bef5fccdf30b43a21982453cfcd5e2960df23
Luke Bennett <hello@lukebennett.com.au>
2021-03-30 20:29:22 +1100

    Add hero prop to layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,13 +6,15 @@ import { SEO } from './seo';
 
 interface LayoutProps {
   children: React.ReactNode;
+  hero?: React.ReactNode;
 }
 
-function Layout({ children }: LayoutProps): React.ReactElement {
+function Layout({ children, hero }: LayoutProps): React.ReactElement {
   return (
     <>
       <SEO />
       <div className="relative flex flex-col min-h-screen font-sans antialiased text-gray-700 bg-white fill-available">
+        {hero || null}
         <Nav />
         <main className="relative flex flex-col flex-1">{children}</main>
         <Footer />
