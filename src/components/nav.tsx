3cd80bdd423503ac819a72bb1f027b1a95bcd1fd
Luke Bennett <hello@lukebennett.com.au>
2021-03-30 20:29:09 +1100

    Style mobile menu

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -3,6 +3,7 @@ import { useLocation } from '@reach/router';
 import { Link } from 'gatsby';
 import * as React from 'react';
 import { HiMenu } from 'react-icons/hi';
+import OutsideClickHandler from 'react-outside-click-handler';
 
 import {
   NavItem,
@@ -16,7 +17,7 @@ function Nav(): React.ReactElement {
   const [isOpen, setIsOpen] = React.useState(false);
   const toggle = () => setIsOpen((prevState) => !prevState);
   const siteSettings: SiteSettings = useSiteSettings();
-  const siteNavigation = useSiteNavigation();
+  const siteNavigation: NavItems = useSiteNavigation();
 
   const navItems = siteNavigation.filter(({ footerOnly }) => !footerOnly);
 
@@ -26,9 +27,9 @@ function Nav(): React.ReactElement {
   const secondHalf = navItems.splice(-half);
 
   return (
-    <div className="sticky inset-x-0 top-0 z-10 bg-white">
+    <header className="sticky inset-x-0 top-0 z-10 bg-white">
       <div className="relative z-20">
-        <nav className="flex flex-wrap items-baseline justify-between px-4 py-5 mx-auto md:justify-center max-w-screen-2xl sm:px-6 sm:py-4 lg:px-8 md:space-x-10">
+        <nav className="flex flex-wrap items-center justify-between px-4 py-5 mx-auto md:items-baseline md:justify-center max-w-screen-2xl sm:px-6 sm:py-4 lg:px-8 md:space-x-10">
           {firstHalf.map((navItem) => (
             <NavLink key={navItem.route.current} navItem={navItem} />
           ))}
@@ -38,7 +39,7 @@ function Nav(): React.ReactElement {
               className="inline-flex transform md:mx-auto md:translate-y-1"
             >
               <span className="sr-only">{siteSettings.title}</span>
-              <Logo aria-hidden className="w-auto h-8 sm:h-16" />
+              <Logo aria-hidden className="w-auto h-12 sm:h-16" />
             </Link>
           </div>
           {secondHalf.map((navItem) => (
@@ -57,8 +58,8 @@ function Nav(): React.ReactElement {
           </div>
         </nav>
       </div>
-      {/* <MobileMenu isOpen={isOpen} toggle={toggle} /> */}
-    </div>
+      <MobileMenu isOpen={isOpen} toggle={toggle} />
+    </header>
   );
 }
 
@@ -69,7 +70,7 @@ function NavLink({ navItem }: { navItem: NavItem }): React.ReactElement | null {
     <Link
       key={navItem.label}
       to={navItem.route.current}
-      className={`text-base font-medium uppercase ${
+      className={`hidden text-base font-medium uppercase md:inline-block ${
         pathname === navItem.route.current ? 'text-teal' : 'text-blue-light'
       } hover:text-gray-900`}
     >
@@ -79,6 +80,32 @@ function NavLink({ navItem }: { navItem: NavItem }): React.ReactElement | null {
 }
 
 function MobileMenu({ isOpen, toggle }) {
+  const siteNavigation: NavItems = useSiteNavigation();
+  const siteSettings: SiteSettings = useSiteSettings();
+
+  const handleEscape = React.useCallback(
+    (event) => {
+      if (event.key === 'Escape') {
+        toggle();
+      }
+    },
+    [toggle]
+  );
+
+  React.useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+          toggle();
+        }
+      });
+    }
+    window.addEventListener('keydown', handleEscape);
+    return () => {
+      window.removeEventListener('keydown', handleEscape);
+    };
+  }, [toggle, handleEscape]);
+
   return (
     <Transition
       show={isOpen}
@@ -90,217 +117,72 @@ function MobileMenu({ isOpen, toggle }) {
       leaveTo="opacity-0 scale-95"
       className="absolute inset-x-0 top-0 z-30 p-2 transition origin-top-right transform md:hidden"
     >
-      <div className="bg-white divide-y-2 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 divide-gray-50">
-        <div className="px-5 pt-5 pb-6 sm:pb-8">
-          <div className="flex items-center justify-between">
-            <div>
-              <img
-                className="w-auto h-8"
-                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                alt="Workflow"
-              />
-            </div>
-            <div className="-mr-2">
-              <button
-                type="button"
+      <OutsideClickHandler onOutsideClick={toggle}>
+        <div className="bg-white divide-y-2 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 divide-gray-50">
+          <div className="px-5 pt-5 pb-6 sm:pb-8">
+            <div className="flex items-center justify-between">
+              <Link
+                to="/"
                 onClick={toggle}
-                className="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                className="block transition duration-150 ease-in-out hover:opacity-75"
               >
-                <span className="sr-only">Close menu</span>
-                {/* Heroicon name: outline/x */}
-                <svg
-                  className="w-6 h-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
+                <span className="sr-only">{siteSettings.title}</span>
+                <Logo className="w-auto h-8" />
+              </Link>
+              <div className="-mr-2">
+                <button
+                  type="button"
+                  onClick={toggle}
+                  className="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
+                  <span className="sr-only">Close menu</span>
+                  {/* Heroicon name: outline/x */}
+                  <svg
+                    className="w-6 h-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
             </div>
           </div>
-          <div className="mt-6 sm:mt-8">
-            <nav>
-              <div className="grid gap-7 sm:grid-cols-2 sm:gap-y-8 sm:gap-x-4">
+          <div className="px-5 py-6">
+            <nav className="grid grid-cols-2 gap-4">
+              {siteNavigation.map(({ id, route, label }) => (
                 <Link
-                  to="/"
-                  className="flex items-center p-3 -m-3 rounded-lg hover:bg-gray-50"
+                  key={id}
+                  to={route.current}
+                  onClick={toggle}
+                  className="text-base font-medium text-gray-900 rounded-md hover:text-gray-700"
                 >
-                  <div className="flex items-center justify-center flex-shrink-0 w-10 h-10 text-white bg-indigo-500 rounded-md sm:h-12 sm:w-12">
-                    {/* Heroicon name: outline/chart-bar */}
-                    <svg
-                      className="w-6 h-6"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                      />
-                    </svg>
-                  </div>
-                  <div className="ml-4 text-base font-medium text-gray-900">
-                    Analytics
-                  </div>
+                  {label}
                 </Link>
-                <Link
-                  to="/"
-                  className="flex items-center p-3 -m-3 rounded-lg hover:bg-gray-50"
-                >
-                  <div className="flex items-center justify-center flex-shrink-0 w-10 h-10 text-white bg-indigo-500 rounded-md sm:h-12 sm:w-12">
-                    {/* Heroicon name: outline/cursor-click */}
-                    <svg
-                      className="w-6 h-6"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
-                      />
-                    </svg>
-                  </div>
-                  <div className="ml-4 text-base font-medium text-gray-900">
-                    Engagement
-                  </div>
-                </Link>
-                <Link
-                  to="/"
-                  className="flex items-center p-3 -m-3 rounded-lg hover:bg-gray-50"
-                >
-                  <div className="flex items-center justify-center flex-shrink-0 w-10 h-10 text-white bg-indigo-500 rounded-md sm:h-12 sm:w-12">
-                    {/* Heroicon name: outline/shield-check */}
-                    <svg
-                      className="w-6 h-6"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
-                      />
-                    </svg>
-                  </div>
-                  <div className="ml-4 text-base font-medium text-gray-900">
-                    Security
-                  </div>
-                </Link>
-                <Link
-                  to="/"
-                  className="flex items-center p-3 -m-3 rounded-lg hover:bg-gray-50"
-                >
-                  <div className="flex items-center justify-center flex-shrink-0 w-10 h-10 text-white bg-indigo-500 rounded-md sm:h-12 sm:w-12">
-                    {/* Heroicon name: outline/view-grid */}
-                    <svg
-                      className="w-6 h-6"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
-                      />
-                    </svg>
-                  </div>
-                  <div className="ml-4 text-base font-medium text-gray-900">
-                    Integrations
-                  </div>
-                </Link>
-              </div>
-              <div className="mt-8 text-base">
-                <Link
-                  to="/"
-                  className="font-medium text-indigo-600 hover:text-indigo-500"
-                >
-                  {' '}
-                  View all products <span aria-hidden="true">→</span>
-                </Link>
-              </div>
+              ))}
             </nav>
+            <div className="mt-6">
+              <a
+                href={`tel:${siteSettings.phoneNumber}`}
+                className="flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white transition duration-150 ease-in-out border border-transparent rounded-md shadow-sm bg-blue hover:bg-blue-light"
+              >
+                {siteSettings.phoneNumber}
+              </a>
+              <p className="mt-6 text-base font-medium text-center text-gray-500">
+                Call us today
+              </p>
+            </div>
           </div>
         </div>
-        <div className="px-5 py-6">
-          <div className="grid grid-cols-2 gap-4">
-            <Link
-              to="/"
-              className="text-base font-medium text-gray-900 rounded-md hover:text-gray-700"
-            >
-              Pricing
-            </Link>
-            <Link
-              to="/"
-              className="text-base font-medium text-gray-900 rounded-md hover:text-gray-700"
-            >
-              Docs
-            </Link>
-            <Link
-              to="/"
-              className="text-base font-medium text-gray-900 rounded-md hover:text-gray-700"
-            >
-              Company
-            </Link>
-            <Link
-              to="/"
-              className="text-base font-medium text-gray-900 rounded-md hover:text-gray-700"
-            >
-              Resources
-            </Link>
-            <Link
-              to="/"
-              className="text-base font-medium text-gray-900 rounded-md hover:text-gray-700"
-            >
-              Blog
-            </Link>
-            <Link
-              to="/"
-              className="text-base font-medium text-gray-900 rounded-md hover:text-gray-700"
-            >
-              Contact Sales
-            </Link>
-          </div>
-          <div className="mt-6">
-            <Link
-              to="/"
-              className="flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700"
-            >
-              Sign up
-            </Link>
-            <p className="mt-6 text-base font-medium text-center text-gray-500">
-              Existing customer?{' '}
-              <Link to="/" className="text-indigo-600 hover:text-indigo-500">
-                Sign in
-              </Link>
-            </p>
-          </div>
-        </div>
-      </div>
+      </OutsideClickHandler>
     </Transition>
   );
 }
