1188beb7c34c7666c08c4f476758a099c5b54de2
Luke Bennett <hello@lukebennett.com.au>
2021-03-30 20:28:37 +1100

    Style footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,215 +2,122 @@ import { Link } from 'gatsby';
 import * as React from 'react';
 
 import config from '../../config.json';
-import { FacebookIcon } from '../icons/facebook';
-import { InstagramIcon } from '../icons/instagram';
-import { TwitterIcon } from '../icons/twitter';
+import { NavItems, useSiteNavigation } from '../hooks/use-site-navigation';
+import { SiteSettings, useSiteSettings } from '../hooks/use-site-settings';
+import { Logo } from '../icons/logo';
+import { ICON_MAP } from '../lib/icon-map';
 
 function Footer(): React.ReactElement {
+  const siteSettings: SiteSettings = useSiteSettings();
+  const siteNavigation: NavItems = useSiteNavigation();
   return (
-    <footer className="bg-white" aria-labelledby="footerHeading">
+    <footer aria-labelledby="footerHeading" className="relative bg-white">
       <h2 id="footerHeading" className="sr-only">
         Footer
       </h2>
       <div className="px-4 py-12 mx-auto max-w-screen-2xl sm:px-6 lg:py-16 lg:px-8">
-        <div className="xl:grid xl:grid-cols-3 xl:gap-8">
-          <div className="space-y-8 xl:col-span-1">
-            <img
-              className="h-10"
-              src="https://tailwindui.com/img/logos/workflow-mark-gray-300.svg"
-              alt="Company name"
-            />
-            <p className="text-base text-gray-500">
-              Making the world a better place through constructing elegant
-              hierarchies.
-            </p>
-            <div className="flex space-x-6">
-              {config.socialLinks.map(({ link, socialNetwork }) => {
-                const ICON_MAP = {
-                  Facebook: FacebookIcon,
-                  Instagram: InstagramIcon,
-                  Twitter: TwitterIcon,
-                };
+        <div className="grid items-start lg:grid-cols-2">
+          <div className="grid gap-8 md:grid-cols-2">
+            <div className="grid col-span-1">
+              <Link
+                to="/"
+                className="max-w-xs text-white transition duration-150 ease-in-out hover:text-gray-300"
+              >
+                <span className="sr-only">{siteSettings.title}</span>
+                <Logo aria-hidden className="fill-current" />
+              </Link>
+            </div>
+          </div>
+          <div className="grid mt-16 lg:mt-0 md:grid-cols-2 lg:border-l lg:border-white lg:border-opacity-25 lg:pl-8">
+            <div>
+              <nav className="space-y-4">
+                {siteNavigation.map(({ label, route, id }) => (
+                  <div key={id} className="text-sm">
+                    <Link
+                      to={route.current}
+                      className="font-medium tracking-wider uppercase transition duration-150 ease-in-out hover:underline hover:text-gray-500"
+                    >
+                      {label}
+                    </Link>
+                  </div>
+                ))}
+              </nav>
+            </div>
+            <div className="space-y-4">
+              <dl className="space-y-4">
+                <div>
+                  <dt className="inline font-semibold tracking-wider">
+                    Phone:{' '}
+                  </dt>
+                  <dd className="inline">
+                    <a
+                      href={`tel:${siteSettings.phoneNumber}`}
+                      className="transition duration-150 ease-in-out hover:text-gray-500 hover:underline"
+                    >
+                      {siteSettings.phoneNumber}
+                    </a>
+                  </dd>
+                </div>
+                <div>
+                  <dt className="inline font-semibold tracking-wider">
+                    Email:{' '}
+                  </dt>
+                  <dd className="inline">
+                    <a
+                      href={`mailto:${siteSettings.email}`}
+                      className="transition duration-150 ease-in-out hover:text-gray-500 hover:underline"
+                    >
+                      {siteSettings.email}
+                    </a>
+                  </dd>
+                </div>
+                <div>
+                  <dt className="inline font-semibold tracking-wider">
+                    Address:{' '}
+                  </dt>
+                  <dd className="inline">
+                    <a
+                      href={siteSettings.address.googleMaps.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="transition duration-150 ease-in-out hover:text-gray-500 hover:underline"
+                    >
+                      {siteSettings.address.streetAddress},{' '}
+                      <span className="inline-block hover:underline">
+                        {siteSettings.address.suburb}
+                      </span>
+                    </a>
+                  </dd>
+                </div>
+              </dl>
+              {siteSettings.socialLinks.map(({ link, socialNetwork }) => {
                 const Icon = ICON_MAP[socialNetwork];
                 return (
                   <a
-                    key={link}
                     href={link}
-                    className="text-gray-400 hover:text-gray-500"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-block text-gray-500 transition duration-150 ease-in-out hover:text-gray-400 focus:text-gray-600"
                   >
                     <span className="sr-only">{socialNetwork}</span>
-                    <Icon className="w-6 h-6" aria-hidden />
+                    <Icon aria-hidden className="w-6 h-6" />
                   </a>
                 );
               })}
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-8 mt-12 xl:mt-0 xl:col-span-2">
-            <div className="md:grid md:grid-cols-2 md:gap-8">
-              <div>
-                <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">
-                  Solutions
-                </h3>
-                <ul className="mt-4 space-y-4">
-                  <li>
-                    <Link
-                      to="/"
-                      className="text-base text-gray-500 hover:text-gray-900"
-                    >
-                      Marketing
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/"
-                      className="text-base text-gray-500 hover:text-gray-900"
-                    >
-                      Analytics
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/"
-                      className="text-base text-gray-500 hover:text-gray-900"
-                    >
-                      Commerce
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/"
-                      className="text-base text-gray-500 hover:text-gray-900"
-                    >
-                      Insights
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="mt-12 md:mt-0">
-                <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">
-                  Support
-                </h3>
-                <ul className="mt-4 space-y-4">
-                  <li>
-                    <Link
-                      to="/"
-                      className="text-base text-gray-500 hover:text-gray-900"
-                    >
-                      Pricing
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/"
-                      className="text-base text-gray-500 hover:text-gray-900"
-                    >
-                      Documentation
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/"
-                      className="text-base text-gray-500 hover:text-gray-900"
-                    >
-                      Guides
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/"
-                      className="text-base text-gray-500 hover:text-gray-900"
-                    >
-                      API Status
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="md:grid md:grid-cols-2 md:gap-8">
-              <div>
-                <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">
-                  Company
-                </h3>
-                <ul className="mt-4 space-y-4">
-                  <li>
-                    <Link
-                      to="/"
-                      className="text-base text-gray-500 hover:text-gray-900"
-                    >
-                      About
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/"
-                      className="text-base text-gray-500 hover:text-gray-900"
-                    >
-                      Blog
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/"
-                      className="text-base text-gray-500 hover:text-gray-900"
-                    >
-                      Jobs
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/"
-                      className="text-base text-gray-500 hover:text-gray-900"
-                    >
-                      Press
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/"
-                      className="text-base text-gray-500 hover:text-gray-900"
-                    >
-                      Partners
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="mt-12 md:mt-0">
-                <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">
-                  Legal
-                </h3>
-                <ul className="mt-4 space-y-4">
-                  <li>
-                    <Link
-                      to="/"
-                      className="text-base text-gray-500 hover:text-gray-900"
-                    >
-                      Claim
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/"
-                      className="text-base text-gray-500 hover:text-gray-900"
-                    >
-                      Privacy
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/"
-                      className="text-base text-gray-500 hover:text-gray-900"
-                    >
-                      Terms
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
         </div>
-        <div className="pt-8 mt-12 border-t border-gray-200">
-          <p className="text-base text-gray-400 xl:text-center">
-            © 2020 Workflow, Inc. All rights reserved.
+        <div className="items-center pt-8 mt-16 border-t border-opacity-25 md:flex md:items-center md:justify-between">
+          <p className="mt-8 text-base md:mt-0 md:order-1">
+            <a
+              href="https://www.phirannodesigns.com.au/"
+              className="transition duration-150 ease-in-out hover:text-gray-500 hover:underline group"
+            >
+              Website by{' '}
+              <span className="font-semibold group-hover:text-gray-500">
+                Phiranno Designs
+              </span>
+            </a>
           </p>
         </div>
       </div>
