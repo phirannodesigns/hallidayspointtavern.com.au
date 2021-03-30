6faaf4b10903d9106e08a8746aeccf9daabb82f7
Luke Bennett <hello@lukebennett.com.au>
2021-03-30 20:29:36 +1100

    Add ICON_MAP

---
+++ src/lib/icon-map.ts
@@ -0,0 +1,9 @@
+import { FacebookIcon } from '../icons/facebook';
+import { InstagramIcon } from '../icons/instagram';
+import { TwitterIcon } from '../icons/twitter';
+
+export const ICON_MAP = {
+  Facebook: FacebookIcon,
+  Instagram: InstagramIcon,
+  Twitter: TwitterIcon,
+};
