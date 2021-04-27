f3d661247c202f95ef8fc4a4a84b8bbd3bb1105c
Luke Bennett <hello@lukebennett.com.au>
2021-04-27 16:29:02 +1000

    Remove transparent variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,11 @@ module.exports = {
       colors: {
         black: {
           DEFAULT: '#2c3239',
-          transparent: 'hsla(212, 13%, 20%, 0.76)',
         },
         cream: '#f5e7b8',
         olive: '#929e64',
         teal: {
           DEFAULT: '#486c6d',
-          transparent: 'hsla(182, 20%, 35%, 0.85)',
         },
         blue: {
           DEFAULT: '#087db0',
