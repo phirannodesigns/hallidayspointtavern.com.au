c51db92d7b57dfe4d3295e408cc85b13f8c011c6
Luke Bennett <hello@lukebennett.com.au>
2021-04-27 11:35:27 +1000

    Add max height to hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,16 +31,18 @@ function IndexPage(): React.ReactElement {
 
 function Hero() {
   return (
-    <div className="aspect-w-16 aspect-h-9">
-      <div className="absolute inset-0 flex">
-        <StaticImage
-          src="../images/landing-hero.jpg"
-          alt=""
-          className="flex-1"
-        />
-      </div>
-      <div className="flex items-center justify-center flex-1 px-4 sm:px-6 lg:px-8">
-        <LogoWhite className="w-full max-w-4xl mx-auto" />
+    <div className="relative max-h-[36rem]">
+      <div className="aspect-w-16 aspect-h-9">
+        <div className="absolute inset-0 flex max-h-[36rem]">
+          <StaticImage
+            src="../images/landing-hero.jpg"
+            alt=""
+            className="flex-1"
+          />
+        </div>
+        <div className="flex items-center justify-center flex-1 px-4 bg-opacity-25 max-h-[36rem] sm:px-6 lg:px-8 bg-teal">
+          <LogoWhite className="w-full max-w-4xl mx-auto" />
+        </div>
       </div>
     </div>
   );
