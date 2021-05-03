a54cad1a56f2286ee3446017f6802b200550e12a
Andrew Phelps <59522519+NOP-slide@users.noreply.github.com>
2021-05-03 16:50:43 +1000

    fix menu and add mobile listbox

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -8,6 +8,9 @@ import {
 } from '@reach/tabs';
 import { graphql, useStaticQuery } from 'gatsby';
 import * as React from 'react';
+import { Fragment } from 'react';
+import { Listbox, Transition } from '@headlessui/react';
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
 
 interface MenuItem {
   id: string;
@@ -48,36 +51,116 @@ function Menu(): React.ReactElement {
     }
   `);
 
-  const { nodes: menuData } = allSanityMenuList;
-  const startingIndex = 0;
+  const { nodes: menuList } = allSanityMenuList;
+  const [tabIndex, setTabIndex] = React.useState(0);
 
   return (
     <article id="menu" className="bg-white">
       <div className="relative w-full px-4 py-24 mx-auto bg-cream max-w-screen-2xl sm:px-6 lg:px-12">
-        <Tabs defaultIndex={startingIndex}>
+        <Tabs
+          index={tabIndex}
+          onChange={(index) => {
+            setTabIndex(index);
+          }}
+        >
           <TabList className="flex flex-col px-4 space-y-4 text-4xl font-black text-black md:space-y-0 md:space-x-4 md:flex-row md:justify-between">
             <div>
               <div className="flex flex-col">
-                <h2 className="px-4 -mb-4 text-xl font-bold text-black md:text-center">
+                <span className="px-4 -mb-4 text-xl font-normal text-black md:text-center">
                   Check out
-                </h2>
-                <h2 className="px-4 py-2 text-4xl font-black text-black">
+                </span>
+                <span className="px-4 py-2 text-4xl font-black text-black">
                   The Menu
-                </h2>
+                </span>
               </div>
             </div>
-            <div>
-              {menuData.map((item, index) => (
+            <div className="block w-full mx-auto sm:hidden">
+              <Listbox value={tabIndex} onChange={setTabIndex}>
+                {({ open }) => (
+                  <>
+                    <div className="relative mt-1">
+                      <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+                        <span className="block truncate">
+                          {menuList[tabIndex].category}
+                        </span>
+                        <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                          <SelectorIcon
+                            className="w-5 h-5 text-gray-400"
+                            aria-hidden="true"
+                          />
+                        </span>
+                      </Listbox.Button>
+                      <Transition
+                        show={open}
+                        as={Fragment}
+                        leave="transition ease-in duration-100"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                      >
+                        <Listbox.Options
+                          static
+                          className="absolute z-10 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+                        >
+                          {menuList.map((menu, index) => (
+                            <Listbox.Option
+                              key={index}
+                              className={({ active }) =>
+                                `${
+                                  active
+                                    ? 'text-amber-900 bg-amber-100'
+                                    : 'text-gray-900'
+                                }
+                          cursor-default select-none relative py-2 pl-10 pr-4`
+                              }
+                              value={index}
+                            >
+                              {({ selected, active }) => (
+                                <>
+                                  <span
+                                    className={`${
+                                      selected ? 'font-medium' : 'font-normal'
+                                    } block truncate`}
+                                  >
+                                    {menu.category}
+                                  </span>
+                                  {selected ? (
+                                    <span
+                                      className={`${
+                                        active
+                                          ? 'text-amber-600'
+                                          : 'text-amber-600'
+                                      }
+                                absolute inset-y-0 left-0 flex items-center pl-3`}
+                                    >
+                                      <CheckIcon
+                                        className="w-5 h-5"
+                                        aria-hidden="true"
+                                      />
+                                    </span>
+                                  ) : null}
+                                </>
+                              )}
+                            </Listbox.Option>
+                          ))}
+                        </Listbox.Options>
+                      </Transition>
+                    </div>
+                  </>
+                )}
+              </Listbox>
+            </div>
+            <div className="hidden sm:block">
+              {menuList.map((item, index) => (
                 <MenuTab key={item.id} item={item} index={index} />
               ))}
             </div>
           </TabList>
           <TabPanels className="grid gap-16 mt-12 bg-white">
-            {menuData.map(({ items, id }, index) => (
+            {menuList.map(({ items, id }, index) => (
               <MenuPanel
                 key={id}
                 dishes={items}
-                menuData={menuData}
+                menuList={menuList}
                 index={index}
               />
             ))}
@@ -115,10 +198,10 @@ function MenuTab({ index, item, ...rest }: MenuTabProps) {
 interface MenuPanelProps {
   index: number;
   dishes: Array<MenuItem>;
-  menuData: Array<MenuList>;
+  menuList: Array<MenuList>;
 }
 
-function MenuPanel({ dishes, index, menuData, ...rest }: MenuPanelProps) {
+function MenuPanel({ dishes, index, menuList, ...rest }: MenuPanelProps) {
   const { selectedIndex } = useTabsContext();
   return (
     <TabPanel
@@ -128,15 +211,17 @@ function MenuPanel({ dishes, index, menuData, ...rest }: MenuPanelProps) {
       {...rest}
     >
       <p className="text-center text-black">
-        {menuData[selectedIndex].description}
+        {menuList[selectedIndex].description}
       </p>
       <div className="grid gap-12 mt-8 md:grid-cols-2">
         {dishes.map(({ id, itemName, description, price }) => (
           <div
             key={id}
             className={`${
-              description === 'Heading' && 'col-span-2'
-            } flex items-start justify-between space-x-16 text-black`}
+              description === 'Heading'
+                ? 'col-span-2'
+                : 'col-span-2 sm:col-span-1'
+            }  flex items-start justify-between space-x-16 text-black`}
           >
             {description !== 'Heading' ? (
               <>
