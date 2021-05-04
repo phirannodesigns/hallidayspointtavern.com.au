0d7e2bd80cf586ce57eb4d6c8ad262af57398f73
Luke Bennett <hello@lukebennett.com.au>
2021-05-04 14:20:57 +1000

    Spread 'rest' prop

--- src/components/form-elements/input.tsx
+++ src/components/form-elements/input.tsx
@@ -8,10 +8,11 @@ interface InputProps {
   label: string;
   name: string;
   type?: 'email' | 'number' | 'password' | 'search' | 'tel';
+  rest?: unknown[];
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ errors, label, name, type = 'text' }, ref) => {
+  ({ errors, label, name, type = 'text', ...rest }, ref) => {
     const hasErrors = Boolean(errors?.[name]);
     return (
       <div>
@@ -25,6 +26,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             ref={ref}
             aria-invalid={hasErrors}
             className="w-full border-transparent focus:ring-blue-500 focus:ring-opacity-50 focus:border-blue-500 focus:border-opacity-50"
+            {...rest}
           />
         </label>
         <ErrorMessage errors={errors} name={name} label={label} />
--- src/components/form-elements/textarea.tsx
+++ src/components/form-elements/textarea.tsx
@@ -8,10 +8,11 @@ type TextareaProps = {
   label: string;
   rows?: number;
   errors: DeepMap<Record<string, unknown>, FieldError>;
+  rest?: unknown[];
 };
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ name, label, rows = 3, errors }, ref) => {
+  ({ name, label, rows = 3, errors, ...rest }, ref) => {
     const hasErrors = Boolean(errors?.[name]);
     return (
       <div>
@@ -25,6 +26,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
             ref={ref}
             aria-invalid={hasErrors}
             className="w-full border-transparent focus:ring-blue-500 focus:ring-opacity-50 focus:border-blue-500 focus:border-opacity-50"
+            {...rest}
           />
         </label>
         <ErrorMessage errors={errors} name={name} label={label} />
