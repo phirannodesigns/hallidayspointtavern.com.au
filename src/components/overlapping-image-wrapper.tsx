d24003c1121a433c2bfa8d29a5145f9bf373f2ff
Luke Bennett <hello@lukebennett.com.au>
2021-05-05 12:02:05 +1000

    Centre images

--- src/components/overlapping-image-wrapper.tsx
+++ src/components/overlapping-image-wrapper.tsx
@@ -16,7 +16,7 @@ function OverlappingImageWrapper({
 }: OverlappingImageWrapperProps): React.ReactElement {
   return (
     <div
-      className={`px-4 py-12 sm:px-6 lg:px-12 lg:py-24 ${OVERLAP_DIRECTION_MAP[overlapDirection]}`}
+      className={`px-4 py-12 sm:px-6 mx-auto lg:px-12 lg:py-24 ${OVERLAP_DIRECTION_MAP[overlapDirection]}`}
     >
       {children}
     </div>
