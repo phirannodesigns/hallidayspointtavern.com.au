0f8d76de86251b1748026bce8070870a606f0578
Luke Bennett <hello@lukebennett.com.au>
2021-05-08 08:19:17 +1000

    Make hero section work better when there is lots of text on small screens

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -9,8 +9,10 @@ function Hero({ backgroundImage, children }: HeroProps): React.ReactElement {
   return (
     <div className="relative max-h-[36rem]">
       <div className="sm:aspect-w-16 sm:aspect-h-9">
-        <div className="flex max-h-[36rem]">{backgroundImage}</div>
-        <div className="absolute inset-0 flex items-center justify-center flex-1 px-4 bg-opacity-25 max-h-[36rem] sm:px-6 lg:px-12 bg-teal">
+        <div className="absolute inset-0 flex max-h-[36rem]">
+          {backgroundImage}
+        </div>
+        <div className="min-h-[16rem] relative flex items-center justify-center flex-1 px-4 bg-opacity-25 max-h-[36rem] sm:px-6 py-12 lg:px-12 bg-teal">
           {children}
         </div>
       </div>
