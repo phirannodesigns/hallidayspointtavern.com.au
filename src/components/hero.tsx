53368ce542a984bc9610924377790d352e19f05b
Luke Bennett <hello@lukebennett.com.au>
2021-05-07 17:46:21 +1000

    Stop hero image collapsing on mobile

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,11 +8,9 @@ interface HeroProps {
 function Hero({ backgroundImage, children }: HeroProps): React.ReactElement {
   return (
     <div className="relative max-h-[36rem]">
-      <div className="aspect-w-16 aspect-h-9">
-        <div className="absolute inset-0 flex max-h-[36rem]">
-          {backgroundImage}
-        </div>
-        <div className="flex items-center justify-center flex-1 px-4 bg-opacity-25 max-h-[36rem] sm:px-6 lg:px-12 bg-teal">
+      <div className="sm:aspect-w-16 sm:aspect-h-9">
+        <div className="flex max-h-[36rem]">{backgroundImage}</div>
+        <div className="absolute inset-0 flex items-center justify-center flex-1 px-4 bg-opacity-25 max-h-[36rem] sm:px-6 lg:px-12 bg-teal">
           {children}
         </div>
       </div>
